d8cbc9dbe8e70928589a85de76fc9748c82b5ec0
Ilija Vukotic <ivukotic@cern.ch>
2018-04-06 22:30:30 -0500

    dbg

--- kube/secrets/xcache-secret.bat
+++ kube/secrets/xcache-secret.bat
@@ -1,1 +1,1 @@
-kubectl create secret generic xcache-cert-secret --from-file=xcache-key=certificates/xcache.key.pem --from-file=xcache-crt-=certificates/xcache.crt.pem
+kubectl create secret generic xcache-cert-secret --from-file=xcache-key.pem=certificates/xcache.key.pem --from-file=xcache-crt.pem=certificates/xcache.crt.pem
--- run_x509_updater.sh
+++ run_x509_updater.sh
@@ -3,35 +3,18 @@
 # Need to install this - it will create and populate directory /etc/grid-security/certificates
 yum install osg-ca-certs voms-clients fetch-crl -y
 
+chmod 400 /root/.globus/xcache-key.pem
+chmod 644 /root/.globus/xcache-crt.pem
+
 while true; do 
+
+  # update proxy
+  voms-proxy-init -key /root/.globus/xcache-key.pem -cert /root/.globus/xcache-crt.pem
+  cp /tmp/x509up_u0 /etc/grid-security/x509up
+  
   sleep 21600
 
   # update crls
   /usr/sbin/fetch-crl
 
-  # update proxy
-  # voms-proxy-init -voms atlas -pwstdin < /afs/cern.ch/user/i/ivukotic/gridlozinka.txt
-
 done
-
-
-# X509_USER_PROXY, X509_CERT_DIR, X509_VOMS_DIR do not have to be defined/provided
-# but then it won't really be useful
-
-# if x509 user proxy is provided mount it in /etc/grid-security/x509up
-
-unset X509_USER_PROXY
-[ -s /etc/grid-security/x509up ] && export X509_USER_PROXY=/etc/grid-security/x509up
-
-# if X509_CERT_DIR is provided mount it in /etc/grid-security/certificates
-unset X509_CERT_DIR
-[ ! -d "$X509_CERT_DIR" ] && export X509_CERT_DIR=/etc/grid-security/certificates
-
-# if X509_VOMS_DIR is provided mount it in /etc/grid-security/vomsdir
-unset X509_VOMS_DIR
-[ ! -d "$X509_VOMS_DIR" ] && export X509_VOMS_DIR=/etc/grid-security/vomsdir
-
-echo $X509_USER_PROXY $X509_CERT_DIR $X509_VOMS_DIR
-
-
-sleep infinity
