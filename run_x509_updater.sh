2a6b9ba0953b40f529eea6dc15bbb38743d86bca
Ilija Vukotic <ivukotic@cern.ch>
2018-04-10 11:58:51 -0500

    adding voms stuff

--- run_x509_updater.sh
+++ run_x509_updater.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Need to install this - it will create and populate directory /etc/grid-security/certificates
-yum install osg-ca-certs voms-clients fetch-crl -y
+yum install osg-ca-certs voms-clients wlcg-voms-atlas fetch-crl -y
 
 chmod 400 /root/.globus/userkey.pem
 chmod 644 /root/.globus/usercert.pem
@@ -9,7 +9,7 @@ chmod 644 /root/.globus/usercert.pem
 while true; do 
 
   # update proxy
-  voms-proxy-init -key /root/.globus/userkey.pem -cert /root/.globus/usercert.pem
+  voms-proxy-init -key /root/.globus/userkey.pem -cert /root/.globus/usercert.pem --voms=atlas
   cp /tmp/x509up_u0 /etc/grid-security/x509up
   
   sleep 21600
