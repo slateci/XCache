c6ec96fb1f18ceec857534e7d5bf27a19785c57b
Ilija Vukotic <ivukotic@cern.ch>
2018-04-13 13:35:18 -0500

    move

--- run_x509_updater.sh
+++ run_x509_updater.sh
@@ -3,13 +3,14 @@
 # Need to install this - it will create and populate directory /etc/grid-security/certificates
 yum install osg-ca-certs voms-clients wlcg-voms-atlas fetch-crl -y
 
-chmod 400 /root/.globus/userkey.pem
-chmod 644 /root/.globus/usercert.pem
+CERTPATH=/etc/grid-certs
+chmod 400 $CERTPATH/userkey.pem
+chmod 644 $CERTPATH/usercert.pem
 
 while true; do 
 
   # update proxy
-  voms-proxy-init -key /root/.globus/userkey.pem -cert /root/.globus/usercert.pem --voms=atlas
+  voms-proxy-init -key $CERTPATH/userkey.pem -cert $CERTPATH/usercert.pem --voms=atlas
   mv /tmp/x509up_u0 /etc/grid-security/x509up
   chown xrootd /etc/grid-security/x509up
 
