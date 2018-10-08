56dcc389ffdbc0a7bf39bbe284f77b4479b45534
Ilija Vukotic <ivukotic@cern.ch>
2018-10-08 13:44:30 -0500

    start Multisite sim.

--- federation/backend/kube/start.bat
+++ federation/backend/kube/start.bat
@@ -1,4 +1,4 @@
-kubectl create secret -n ml-usatlas-org generic mg-config --from-file=mgconf=secrets/mg-config.json
-kubectl create secret -n ml-usatlas-org generic cert-secret --from-file=key=secrets/certificates/ml-front.key.pem --from-file=cert=secrets/certificates/ml-front.cert.cer
+kubectl create secret -n xcache generic config --from-file=config=config.json
+kubectl create secret -n xcache generic cert-secret --from-file=key=secrets/certificates/xcache-backend.key.pem --from-file=cert=secrets/certificates/xcache-backend.cert.cer
 
 kubectl create -f deployment.yaml
