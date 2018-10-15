1d7fea8b2cdb5bebf898c333b4f908b7027328ec
Ilija Vukotic <ivukotic@cern.ch>
2018-10-15 14:43:40 -0500

    plots

--- analytics/Stresser.py
+++ analytics/Stresser.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
-
+import numpy as np
 import requests
 import json
 import pandas as pd
 
-#service = "http://192.170.227.234:80"
+# service = "http://192.170.227.234:80"
 service = "http://localhost:80"
 
 sites = ['AGLT2', 'MWT2']
@@ -14,6 +14,9 @@ GB = 1024 * 1024 * 1024
 TB = 1024 * GB
 PB = 1024 * TB
 
+
+# ### Import data
+
 all_accesses = []
 for si, site in enumerate(sites):
     print('Loading:', site)
@@ -21,70 +24,104 @@ for si, site in enumerate(sites):
     all_accesses[si]['site'] = 'xc_' + site
     # print(all_accesses[si].head())
     print(all_accesses[si].filesize.count(), "files")
+    print(all_accesses[si].index.unique().shape[0], " unique files")
     print(all_accesses[si].filesize.sum() / PB, "PB")
     print(all_accesses[si].filesize.mean() / GB, "GB avg. file size")
+    print('----------------------------')
 
 all_data = pd.concat(all_accesses).sort_values('transfer_start')
 
-print('---------- data merged -----------')
-print(all_data.shape[0], 'files')
+print('---------- merged data -----------')
+print(all_data.shape[0], 'files\t\t', all_data.index.unique().shape[0], 'unique files')
 print(all_data.filesize.sum() / PB, "PB")
 print(all_data.filesize.mean() / GB, "GB avg. file size")
-# print(all_data.head(100))
+
+# ### running requests
 
 print('---------- start requests ----------')
-
+acs = []
+dac = []
 accesses = [0, 0, 0, 0]
 dataaccc = [0, 0, 0, 0]
 count = 0
 payload = []
-for index, row in all_data.iterrows():
-    if count > 31000:
-        break
-    fs = row['filesize']
-    payload.append({'filename': index, 'site': row['site'], 'filesize': fs, 'time': row['transfer_start']})
-    # print(payload)
-    try:
-        if count % 100 == 1:
-            r = requests.post(service + '/simulate', json=payload)
-            if r.status_code != 200:
-                print(r)
-            accs = r.json()
-            for i, j in enumerate(accs['counts']):
-                accesses[i] += int(j)
-                dataaccc[i] += accs['sizes'][i]
-            payload = []
-    except requests.exceptions.RequestException as e:
-        print(e)
-
-    if not count % 1000:
-        print(count, 'accesses finished.', accesses, dataaccc)
-    count += 1
+with requests.Session() as session:
+    for index, row in all_data.iterrows():
+        if count > 3000000000:
+            break
+        fs = row['filesize']
+        payload.append({'filename': index, 'site': row['site'], 'filesize': fs, 'time': row['transfer_start']})
+        # print(payload)
+        try:
+            if count % 100 and count > 0:
+                r = session.post(service + '/simulate', json=payload)
+                if r.status_code != 200:
+                    print(r)
+                accs = r.json()
+                for i, j in enumerate(accs['counts']):
+                    accesses[i] += int(j)
+                    dataaccc[i] += accs['sizes'][i]
+                payload = []
+        except requests.exceptions.RequestException as e:
+            print(e)
+        if not count % 5000 and count > 0:
+            # print(count, accesses, dataaccc)
+            acs.append(accesses.copy())
+            dac.append(dataaccc.copy())
+            pacce = []
+            pdata = []
+            for i in range(len(accesses)):
+                pacce.append(accesses[i] / sum(accesses))
+                pdata.append(dataaccc[i] / sum(dataaccc))
+            print(count, pacce, pdata)
+        count += 1
 
 print('final: ', accesses, dataaccc)
+
+accdf = pd.DataFrame(acs)
+dacdf = pd.DataFrame(dac)
+
+dacdf = dacdf / (1024 * 1024 * 1024 * 1024)
+
+
+# ### ploting results
+
+accdf.columns = ['level 1', 'level 2', 'level 3', 'origin']
+dacdf.columns = ['level 1', 'level 2', 'level 3', 'origin']
+
+fig, axs = plt.subplots(nrows=2, ncols=1, figsize=(8, 10))
+
+# plt.subplot(211)
+accdf.plot(ax=axs[0])
+axs[0].set_ylabel('hits')
+axs[0].set_xlabel('reqeusts [x1000]')
+axs[0].legend()
+
+dacdf.plot(ax=axs[1])
+axs[1].set_ylabel('data delivered [TB]')
+axs[1].set_xlabel('reqeusts [x1000]')
+axs[1].legend()
+
+# plt.show()
+
+fig.savefig('filling_up.png')
+
+
+# ### Network states
 
 res = requests.get(service + '/status')
 status = json.loads(res.json())
 # print(status)
+tp = []
 for site in status:
-    print(site[0])
-    print(site[1])
+    tp.append([site[0], site[1]['requests_received'], site[1]['files_delivered'], site[1]['data_delivered'] / (1024 * 1024 * 1024 * 1024)])
 
-asdf
+sites = pd.DataFrame(tp)
+sites.columns = ['xcache', 'requests', 'hits', 'data delivered']
+sites = sites[sites.requests != 0]
+sites.head(20)
 
-data = (25, 32, 34, 20, 25)
+fig, ax = plt.subplots(figsize=(8, 8))
 
-ind = np.arange(len(accesses))  # the x locations for the groups
-width = 0.35  # the width of the bars
-
-fig, ax = plt.subplots()
-rects1 = ax.bar(ind - width / 2, accesses, width, color='SkyBlue', label='cache hits')
-rects2 = ax.bar(ind + width / 2, data, width, color='IndianRed', label='data delivered from cache')
-
-# Add some text for labels, title and custom x-axis tick labels, etc.
-ax.set_ylabel('Scores')
-ax.set_title('Scores by group and gender')
-ax.set_xticks(ind)
-ax.set_xticklabels(('L1', 'L2', 'L3'))
-ax.legend()
-plt.show()
+sites.plot(x="xcache", y=["requests", "hits", "data delivered"], kind="bar", ax=ax, secondary_y='data delivered')
+fig.savefig('xcache_sites.png')
