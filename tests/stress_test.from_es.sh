babcb6cb8e66d4e4d7c72a1c42c55db29b8a17d5
Ilija Vukotic <ivukotic@cern.ch>
2018-10-27 22:02:25 -0500

    fixes

--- tests/stress_test.from_es.sh
+++ tests/stress_test.from_es.sh
@@ -5,18 +5,9 @@
 # X509_USER_PROXY, X509_CERT_DIR, X509_VOMS_DIR do not have to be defined/provided
 # but then it won't really be useful
 
-# if x509 user proxy is provided mount it in /var/run/x509up
-
-unset X509_USER_PROXY
-[ -s /var/run/x509up ] && export X509_USER_PROXY=/var/run/x509up
-
-# if X509_CERT_DIR is provided mount it in /etc/grid-security/certificates
-unset X509_CERT_DIR
-[ ! -d "$X509_CERT_DIR" ] && export X509_CERT_DIR=/etc/grid-security/certificates
-
-# if X509_VOMS_DIR is provided mount it in /etc/grid-security/vomsdir
-unset X509_VOMS_DIR
-[ ! -d "$X509_VOMS_DIR" ] && export X509_VOMS_DIR=/etc/grid-security/vomsdir
+export X509_USER_PROXY=/etc/grid-security/x509up
+export X509_CERT_DIR=/etc/grid-security/certificates
+export X509_VOMS_DIR=/etc/grid-security/vomsdir
 
 echo $X509_USER_PROXY $X509_CERT_DIR $X509_VOMS_DIR
 
@@ -28,7 +19,7 @@ export TCMALLOC_RELEASE_RATE=10
 #SERVER="https://xcache.org"
 SERVER=$1
 
-# XCACHE_SERVER='https://fax.mwt2.org'
+# XCACHE_SERVER='https://fax.mwt2.org//'
 XCACHE_SERVER=$2
 
 NFILES=10
@@ -47,7 +38,7 @@ do
     for ((i=0;i<${#fns[@]};++i)); do
         DA=$(date)
         printf "$DA copying %s\n" "${fns[i]}" "${pths[i]}"
-        xrdcp -f $XCACHE_SERVER/${pths[i]} /dev/null
+        xrdcp -f $XCACHE_SERVER${pths[i]} /dev/null
     done
 
 done
--- tests/stress_test.sh
+++ tests/stress_test.sh
@@ -3,18 +3,9 @@
 # X509_USER_PROXY, X509_CERT_DIR, X509_VOMS_DIR do not have to be defined/provided
 # but then it won't really be useful
 
-# if x509 user proxy is provided mount it in /var/run/x509up
-
-unset X509_USER_PROXY
-[ -s /var/run/x509up ] && export X509_USER_PROXY=/var/run/x509up
-
-# if X509_CERT_DIR is provided mount it in /etc/grid-security/certificates
-unset X509_CERT_DIR
-[ ! -d "$X509_CERT_DIR" ] && export X509_CERT_DIR=/etc/grid-security/certificates
-
-# if X509_VOMS_DIR is provided mount it in /etc/grid-security/vomsdir
-unset X509_VOMS_DIR
-[ ! -d "$X509_VOMS_DIR" ] && export X509_VOMS_DIR=/etc/grid-security/vomsdir
+export X509_USER_PROXY=/etc/grid-security/x509up
+export X509_CERT_DIR=/etc/grid-security/certificates
+export X509_VOMS_DIR=/etc/grid-security/vomsdir
 
 echo $X509_USER_PROXY $X509_CERT_DIR $X509_VOMS_DIR
 
