dcd8ee350d22a7762e646771bc82217f20ce7752
Ilija Vukotic <ivukotic@cern.ch>
2018-11-01 10:26:13 -0500

    moving things out

--- federation/backend/kube/start.bat
+++
@@ -1,4 +0,0 @@
-kubectl create secret -n xcache generic config --from-file=config=config.json
-kubectl create secret -n xcache generic cert-secret --from-file=key=secrets/certificates/xcache-backend.key.pem --from-file=cert=secrets/certificates/xcache-backend.cert.cer
-
-kubectl create -f deployment.yaml
