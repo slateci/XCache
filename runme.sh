b14668228e416466ecfedfd1ef8dac7f560a1a93
Ilija Vukotic <ivukotic@cern.ch>
2018-11-27 13:53:36 -0600

    not chown if not needed

--- runme.sh
+++ runme.sh
@@ -3,7 +3,8 @@
 # make cache space owned by xrootd user
 mkdir -p /cache/xrdcinfos/
 mkdir -p /cache/datafiles/
-chown -R xrootd:xrootd /cache
+
+if [ $(stat -c "%U:%G" /cache ) != "xrootd:xrootd" ]; then  chown -R xrootd:xrootd /cache; fi
 
 # sleep long enough to get x509 things set up.
 echo "Waiting 2 min for other containers to start."
