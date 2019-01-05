e44e8379cf8338408c4dfae7591c51f638352850
Ilija Vukotic <ivukotic@cern.ch>
2019-01-04 22:55:09 -0600

    cleanup

--- analytics/SchedulingWithCache/OPAO_sim.py
+++ analytics/SchedulingWithCache/OPAO_sim.py
@@ -22,7 +22,7 @@ KINDS = ['prod']  # anal
 DONT_CACHE = []
 data = ou.load_data(PERIODS, KINDS)
 
-data = data[100:150]
+data = data[101:1000]
 
 # creating jobs
 counter = 0
@@ -35,20 +35,6 @@ print('total tasks created:', counter)
 
 grid.process_jobs()
 grid.plot_stats()
-
-#     if not count % step and count > 0:
-#         # print(count, accesses, dataaccc)
-#         acs.append(accesses.copy())
-#         dac.append(dataaccc.copy())
-#         pacce = []
-#         pdata = []
-#         for i in range(len(accesses)):
-#             pacce.append(accesses[i] / sum(accesses))
-#             pdata.append(dataaccc[i] / sum(dataaccc))
-#         print(count, pacce, pdata)
-
-#     if row.site not in all_sites:
-#         continue
 
 #     fs = row.filesize
 #     ts = row.transfer_start
@@ -79,78 +65,3 @@ grid.plot_stats()
 #         accesses[3] += 1
 #         dataaccc[3] += fs
 #         continue
-
-
-# print('final: ', accesses, dataaccc)
-
-
-# accdf = pd.DataFrame(acs)
-# dacdf = pd.DataFrame(dac)
-
-# dacdf = dacdf / TB
-
-
-# # ### ploting results
-
-# accdf.columns = ['level 1', 'level 2',  'origin']
-# dacdf.columns = ['level 1', 'level 2',  'origin']
-
-# fig, axs = plt.subplots(nrows=2, ncols=2, figsize=(12, 10))
-# fig.suptitle(title, fontsize=18)
-
-# accdf.plot(ax=axs[0][0])
-# axs[0][0].set_ylabel('hits')
-# axs[0][0].set_xlabel('requests [x' + str(step) + ']')
-# axs[0][0].legend()
-
-# dacdf.plot(ax=axs[1][0])
-# axs[1][0].set_ylabel('data delivered [TB]')
-# axs[1][0].set_xlabel('requests [x' + str(step) + ']')
-# axs[1][0].legend()
-
-# accdf = accdf.div(accdf.sum(axis=1), axis=0)
-# dacdf = dacdf.div(dacdf.sum(axis=1), axis=0)
-
-# accdf.plot(ax=axs[0][1])
-# axs[0][1].set_ylabel('hits [%]')
-# axs[0][1].set_xlabel('requests [x' + str(step) + ']')
-# axs[0][1].grid(axis='y')
-# axs[0][1].legend()
-
-# dacdf.plot(ax=axs[1][1])
-# axs[1][1].set_ylabel('data delivered [%]')
-# axs[1][1].set_xlabel('requests [x' + str(step) + ']')
-# axs[1][1].grid(axis='y')
-# axs[1][1].legend()
-
-# # plt.show()
-
-# fig.savefig('filling_up_' + output + '.png')
-
-
-# # Network states
-
-# tp = []
-# st = pd.DataFrame()
-# for site in all_sites:
-#     s = all_sites[site]
-#     si = [site.replace('xc_', ''), s.requests, s.hits, s.data_asked_for / TB, s.data_from_cache / TB]
-#     if s.requests > 0 and site != 'Origin':
-#         st = pd.concat([st, s.get_servers_stats()])
-#     tp.append(si)
-#     if s.requests > 0:
-#         s.plot_throughput()
-
-# print(st.groupby(['site']).mean())
-
-# sites = pd.DataFrame(tp)
-# sites.columns = ['xcache', 'requests', 'hits', 'data asked for', 'delivered from cache']
-# sites = sites[sites.requests != 0]
-# sites.set_index('xcache', drop=True, inplace=True)
-# print(sites.head(20))
-
-# fig, ax = plt.subplots(figsize=(8, 8))
-# fig.suptitle(title, fontsize=18)
-# sites.plot(kind="bar", ax=ax, secondary_y=['data asked for', 'delivered from cache'])
-# ax.right_ax.set_ylabel('[TB]')
-# fig.savefig('xcache_sites_' + output + '.png')
