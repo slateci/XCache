0bc529f0f96c328f9c53de84478db5e79a2fc4fe
Ilija Vukotic <ivukotic@cern.ch>
2019-05-09 11:26:53 -0500

    still not understood.

--- updateAGISstatus.sh
+++ updateAGISstatus.sh
@@ -5,6 +5,8 @@
 # protocol_id can be seen at the end of the link normally used to edit protocol eg. http://atlas-agis.cern.ch/agis/serviceprotocol/edit/433/
 # new_status can be: ACTIVE or DISABLED  
 
+
+echo "UPDATING AGIS STATUS endpoint: -$1- new status: -$2-"
 curl -k --cert /etc/grid-certs/usercert.pem --cert-type PEM --key /etc/grid-certs/userkey.pem --key-type PEM "https://atlas-agis-api.cern.ch/request/serviceprotocol/update/?json&id=$1&state=$2&state_comment=Automatic+update"
 
 # to generate cert and key in pem format
