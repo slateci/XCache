fe6058c3f771384a833b1acaf629bd50989587f2
Ilija Vukotic <ivukotic@cern.ch>
2019-07-02 08:56:36 -0500

    added life to proxy

--- run_x509_updater.sh
+++ run_x509_updater.sh
@@ -8,11 +8,12 @@ CERTPATH=/etc/grid-certs
 while true; do 
 
   # update proxy
-  voms-proxy-init -key $CERTPATH/userkey.pem -cert $CERTPATH/usercert.pem --voms=atlas
+  voms-proxy-init -valid 24:0 -key $CERTPATH/userkey.pem -cert $CERTPATH/usercert.pem --voms=atlas
   mv /tmp/x509up_u0 /etc/grid-security/x509up
   chown xrootd /etc/grid-security/x509up
+  export X509_USER_PROXY=/etc/grid-security/x509up 
 
-  sleep 21600
+  sleep 82800
 
   # update crls
   /usr/sbin/fetch-crl
