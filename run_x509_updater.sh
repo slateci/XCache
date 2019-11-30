b88aa56178b375e509cc0358bd0aacc15b2656f1
Ilija Vukotic <ivukotic@cern.ch>
2019-11-30 16:24:05 -0500

    longer proxy

--- run_x509_updater.sh
+++ run_x509_updater.sh
@@ -9,7 +9,7 @@ while true; do
   voms-proxy-init -valid 96:0 -key $CERTPATH/userkey.pem -cert $CERTPATH/usercert.pem --voms=atlas
   chown xrootd /etc/proxy/x509up 
 
-  sleep 82800
+  sleep 338400‬
 
   # update crls
   /usr/sbin/fetch-crl
