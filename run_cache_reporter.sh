eb282d481407de28f290f9f0206ec8d862b647d3
Ilija Vukotic <ivukotic@cern.ch>
2020-07-27 11:28:15 -0500

    added stats reporting

--- run_cache_reporter.sh
+++ run_cache_reporter.sh
@@ -3,6 +3,17 @@
 # This one has the data volume mounted and periodically collect information from
 # .cinfo files, aggregate it and report to ES.
 
+COUNTER=0
+for dir in /xcache-data_*
+do
+    echo "Found ${dir}."
+    let COUNTER=COUNTER+1
+    echo "exporting it as DISK_${COUNTER}"
+    export DISK_${COUNTER}=${dir}
+done
+
+/stats.py &
+
 while true; do 
 
   /reporter.py
